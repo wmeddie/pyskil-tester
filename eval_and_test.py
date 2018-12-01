0227cdd5e109ff348175a7ff1085b2fd0c433b2e
Eduardo Gonzalez <me@wm-eddie.com>
2018-12-02 01:08:49 +0900

    Update code to 0.2.5.

--- eval_and_test.py
+++ eval_and_test.py
@@ -28,7 +28,7 @@ experiment = skil.get_experiment_by_id(work_space, skil_conf['experiment_id'])
 
 model = None
 if sys.argv[1] == '--id':
-    model = skil.get_model_by_id(experiment, sys.argv[2])
+    model = skil.get_model_by_id(None, experiment, sys.argv[2]) # TODO: Doesn't work in 0.2.5.
     model.name = 'mnist'
 elif sys.argv[1] == '--file':
     model = SkilModel(sys.argv[2], name='mnist',  id=str(uuid.uuid1()), experiment=experiment)
@@ -57,25 +57,30 @@ x_test = x_test.reshape(10000, 784)
 x_test = x_test.astype('float32')
 x_test /= 255
 
+num_correct = 0
 for i in xrange(10000):
     digit = x_test[i]
     label = y_test[i]
 
-    #result = service.predict_single(digit) Will be fixed in 0.2.5
+    result = service.predict_single(digit)
 
     # Using swagger API:
-    req = skil_client.Prediction(
-        id=str(uuid.uuid1()), 
-        needs_pre_processing=False,
-        prediction=service._indarray(np.expand_dims(digit, 0)))
+    #req = skil_client.Prediction(
+    #    id=str(uuid.uuid1()), 
+    #    needs_pre_processing=False,
+    #    prediction=service._indarray(np.expand_dims(digit, 0)))
 
-    res = skil_server.api.predict(
-        req, 
-        deployment_name=deployment.name, 
-        model_name=service.model_name,
-        version_name='default')
-    result = np.asarray(res.prediction.data).reshape(res.prediction.shape)
+    #res = skil_server.api.predict(
+    #    req, 
+    #    deployment_name=deployment.name, 
+    #    model_name=service.model_name,
+    #    version_name='default')
+    #result = np.asarray(res.prediction.data).reshape(res.prediction.shape)
 
     predicted = np.argmax(result)
 
+    if predicted == label:
+        num_correct += 1
     print("Predicted: %s Actual: %s Same: %s" % (predicted, label, predicted == label))
+
+print("Test Accuracy: %s" % (float(num_correct) / 10000,))
