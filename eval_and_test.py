656c53bf7c25a8b963dcaf8d6e36abd495b59926
Eduardo Gonzalez <me@wm-eddie.com>
2018-12-01 19:02:14 +0900

    Switch to swagger client temporarily.

--- eval_and_test.py
+++ eval_and_test.py
@@ -9,7 +9,8 @@ import json
 import numpy as np
 
 import skil
-from skil import Skil, WorkSpace, Experiment, Deployment, Model as SkilModel
+from skil import Skil, WorkSpace, Experiment, Deployment, Model as SkilModel, Service
+import skil_client
 
 import uuid
 
@@ -28,8 +29,9 @@ experiment = skil.get_experiment_by_id(work_space, skil_conf['experiment_id'])
 model = None
 if sys.argv[1] == '--id':
     model = skil.get_model_by_id(experiment, sys.argv[2])
+    model.name = 'mnist'
 elif sys.argv[1] == '--file':
-    model = SkilModel(sys.argv[2], name=sys.argv[2],  id=str(uuid.uuid1()), experiment=experiment)
+    model = SkilModel(sys.argv[2], name='mnist',  id=str(uuid.uuid1()), experiment=experiment)
 else:
     print('Usage: python eval_and_test.py --id [model_id] | --file [path]')
     sys.exit(1)
@@ -43,7 +45,12 @@ if skil_conf['deployment_id'] == '':
 else:
     deployment = skil.get_deployement_by_id(skil_server, skil_conf['deployment_id'])
 
-service = model.deploy(deployment=deployment)
+service = None
+try:
+    service = model.deploy(deployment=deployment)
+except Exception:
+    print('Service with name already exists reusing...')
+    service = Service(skil_server, "mnist", deployment, {})
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_test = x_test.reshape(10000, 784)
@@ -53,8 +60,22 @@ x_test /= 255
 for i in xrange(10000):
     digit = x_test[i]
     label = y_test[i]
-    result = service.predict_single(digit)
-    predicted = np.argmax(result)
-    actual = np.argmax(label)
 
-    print("Predicted: %s Actual: %s Same: %s" % (predicted, actual, predicted == actual))
+    #result = service.predict_single(digit) Will be fixed in 0.2.5
+
+    # Using swagger API:
+    req = skil_client.Prediction(
+        id=str(uuid.uuid1()), 
+        needs_pre_processing=False,
+        prediction=service._indarray(np.expand_dims(digit, 0)))
+
+    res = skil_server.api.predict(
+        req, 
+        deployment_name=deployment.name, 
+        model_name=service.model_name,
+        version_name='default')
+    result = np.asarray(res.prediction.data).reshape(res.prediction.shape)
+
+    predicted = np.argmax(result)
+
+    print("Predicted: %s Actual: %s Same: %s" % (predicted, label, predicted == label))
